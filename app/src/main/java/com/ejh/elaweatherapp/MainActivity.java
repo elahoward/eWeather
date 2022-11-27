2b80952e5a98c953b38343c965d3b6bf8a2ee05f
elahoward <ehoward@tfs.ca>
2022-11-27 13:47:20 -0500

    .....

--- app/src/main/java/com/ejh/elaweatherapp/MainActivity.java
+++ app/src/main/java/com/ejh/elaweatherapp/MainActivity.java
@@ -8,9 +8,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,25 +21,32 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     TextView date, temp, city, desc;
+    private RequestQueue RQ;
 
     ImageView image;
     String ville= "Toronto";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RQ= Volley.newRequestQueue(this);
         date=findViewById(R.id.date);
         temp=findViewById(R.id.temp);
         city=findViewById(R.id.city);
         desc=findViewById(R.id.desc);
+        afficher();
 
     }
 
     public void afficher (){
+
         String url = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appId=8202c79f5d3d03e92863ebbf770b93d9&units=metric";
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url, null, new Response.Listener<JSONObject>() {
             @Override
+
             public void onResponse(JSONObject response) {
                 try {
                     JSONObject mainobject = response.getJSONObject("main");
@@ -54,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
             }
         });
+        RQ.add(jsonObjectRequest);
 
         }
     }
